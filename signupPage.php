eb61c1276e077af549a8c2e879b254e8a24a73db
Kristina Bondarenko <bondarenkokristina27@gmail.com>
2021-10-26 11:19:55 -0400

    created signup page

---
+++ signupPage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title> Welcome page </title>
+</head>
+<body>
+<section class="forms">
+    <div class="wrapper">
+        <div class="signup">
+            <h4>SIGN UP</h4>
+            <form action="signup.php" method="post">
+            <input type="text" name="user" placeholder="Username"><br></br>
+            <input type="password" name="pwd" placeholder="Password"><br> </br>
+            <input type="password" name="pwdrepeat" placeholder="Repeat Password"><br> </br>
+            <button type="submit" name="submit">Sign Up</button>
+            </form>
+        </div>
+        
+
+    </div>
+   
+</section>
+
+
+<body>
+</html>
